257f12737d4a6808b24505992ab1f2296f171a4a
neuecc <ils@neue.cc>
2015-01-13 23:43:10 +0900

    coordinate VISX command package

---
+++ OpenOnGitHub/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OpenOnGitHub
+{
+    static class PackageGuids
+    {
+        public const string guidOpenOnGitHubPkgString = "465b40b6-311a-4e37-9556-95fced2de9c6";
+        public const string guidOpenOnGitHubCmdSetString = "a674aaec-a6f5-4df0-9749-e7bef776df5d";
+
+        public static readonly Guid guidOpenOnGitHubCmdSet = new Guid(guidOpenOnGitHubCmdSetString);
+    };
+
+    static class PackageCommanddIDs
+    {
+        public const uint OpenMaster = 0x100;
+        public const uint OpenBranch = 0x200;
+        public const uint OpenRevision = 0x300;
+    };
+}
---
+++ OpenOnGitHub/OpenOnGitHubPackage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.ComponentModel.Design;
+using Microsoft.Win32;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+
+namespace OpenOnGitHub
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // TODO:versions
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.guidOpenOnGitHubPkgString)]
+    [ProvideAutoLoad(UIContextGuids80.SolutionExists)]
+    public sealed class OpenOnGitHubPackage : Package
+    {
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (mcs != null)
+            {
+                foreach (var item in new[] { PackageCommanddIDs.OpenMaster, PackageCommanddIDs.OpenBranch, PackageCommanddIDs.OpenRevision })
+                {
+                    var menuCommandID = new CommandID(PackageGuids.guidOpenOnGitHubCmdSet, (int)item);
+                    var menuItem = new OleMenuCommand(ExecuteCommand, menuCommandID);
+                    menuItem.BeforeQueryStatus += MenuItem_BeforeQueryStatus;
+                    mcs.AddCommand(menuItem);
+                }
+            }
+        }
+
+        private void MenuItem_BeforeQueryStatus(object sender, EventArgs e)
+        {
+            var command = (OleMenuCommand)sender;
+
+            // TODO:change text   
+            command.Enabled = false;
+        }
+
+        private void ExecuteCommand(object sender, EventArgs e)
+        {
+            var command = (OleMenuCommand)sender;
+
+            // TODO:execute
+        }
+    }
+}
